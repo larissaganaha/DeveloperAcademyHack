7d91c0d20cf5c1278c8b50d8ce8b4846ae182285
Julianny Favinha <julianny.favinha@gmail.com>
2018-08-24 20:38:57 -0300

    Fix relevant images of sinpLog and reportLog

--- SiriusDoctor/Presentation/Appointment/CustomCells/RelevantImagesTableViewCell.swift
+++ SiriusDoctor/Presentation/Appointment/CustomCells/RelevantImagesTableViewCell.swift
@@ -21,7 +21,6 @@ class RelevantImagesTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 }
 
--- SiriusDoctor/Presentation/TestFirebaseViewController.swift
+++ SiriusDoctor/Presentation/TestFirebaseViewController.swift
@@ -39,13 +39,13 @@ class TestFirebaseViewController: UIViewController {
         let pac3 = Pacient(ID: "155758", name: "Luanda Silva", address: "Rua Alberto de Salvo, 90 - Jardim Santa Genebra II (Barao Geraldo), Campinas - SP", telephone: "+55 (14) 96664-8000", imageURL: nil, bornDate: formatter.date(from: "1995/06/02")!, height: 1.80, weight: 100.0, drink: true, hipertension: false, diabetes: true, smoking: false)
         pacientService.savePacient(pac3)
         
-        let app1 = Appointment(pacient: pac1, scheduledTime: formatter.date(from: "2018/12/08")!, transcript: "", sinptomLog: nil, reportLog: nil)
+        let app1 = Appointment(pacient: pac1, scheduledTime: formatter.date(from: "2018/12/08")!, transcript: "", sinptomLog: DataLog(date: Date(), images: ["https://www.skinmdpllc.com/sbtemplates/sbcommon/images/blog/Melanoma.jpg", "https://www.fotosantesedepois.com/wp-content/uploads/2014/07/Sintomas-de-Alergia-ao-Sol.jpg"], texts: ["dor de estômago"]), reportLog: DataLog(date: Date(), images: [], texts: []))
         appointmentService.saveAppointment(app1)
         
-        let app2 = Appointment(pacient: pac2, scheduledTime: formatter.date(from: "2019/01/03")!, transcript: "", sinptomLog: nil, reportLog: nil)
+        let app2 = Appointment(pacient: pac2, scheduledTime: formatter.date(from: "2019/01/03")!, transcript: "", sinptomLog: DataLog(date: Date(), images: ["http://s2.glbimg.com/3DqkyxE0mubo8zZ3wGZ6bOuGMtUuhCfE-UePrWhWJXoeFijuJJyy22W7__T6PeaS/s.glbimg.com/es/ge/f/original/2012/10/04/pe_da_mayara.jpg", "http://4.bp.blogspot.com/-rXmxiV3Y8BY/VaFdGQ84reI/AAAAAAAADB0/IYpe8iGZ8K0/s1600/11693208_1018902954796060_33698806_n.jpg"], texts: ["bati o pé gente olha que horror!"]), reportLog: DataLog(date: Date(), images: [], texts: []))
         appointmentService.saveAppointment(app2)
         
-        let app3 = Appointment(pacient: pac3, scheduledTime: formatter.date(from: "2018/10/31")!, transcript: "", sinptomLog: nil, reportLog: nil)
+        let app3 = Appointment(pacient: pac3, scheduledTime: formatter.date(from: "2018/10/31")!, transcript: "", sinptomLog: DataLog(date: Date(), images: ["https://tudoela.com/wp-content/uploads/2018/04/hematoma-subungueal-2.jpg"], texts: ["bati o pé gente olha que horror!"]), reportLog: DataLog(date: Date(), images: [], texts: []))
         appointmentService.saveAppointment(app3)
         
         appointmentService.getAllAppointments { (apps) in
