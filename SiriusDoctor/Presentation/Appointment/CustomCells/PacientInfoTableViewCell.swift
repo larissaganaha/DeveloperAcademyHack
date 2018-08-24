39d6fe3878d316ba05d270c6585fbd6ec4cffd33
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2018-08-24 14:49:21 -0300

    ups

---
+++ SiriusDoctor/Presentation/Appointment/CustomCells/PacientInfoTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PacientTableViewCell.swift
+//  SiriusDoctor
+//
+//  Created by Giovani Nascimento Pereira on 24/08/18.
+//  Copyright © 2018 Giovani Nascimento Pereira. All rights reserved.
+//
+
+import UIKit
+
+class PacientTableViewCell: UITableViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
