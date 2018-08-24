44fc7a7cf7d614a5f693aae3872bd1ebac9e50b3
Bianca Itiroko <itirokob@gmail.com>
2018-08-24 16:58:45 -0300

    Bug fixed do Appointment com os sinps e reports

--- Sirius/Model/Appointment.swift
+++ Sirius/Model/Appointment.swift
@@ -12,12 +12,12 @@ class Appointment: PersistenceObject {
     var pacient: Pacient
     var scheduledTime: Date
     var transcript: String
-    var sinptomLog: DataLog
-    var reportLog: DataLog
+    var sinptomLog: DataLog?
+    var reportLog: DataLog?
     var isActive: Bool = true
     var dictInfo: [AnyHashable: Any] = [:]
     
-    init (pacient: Pacient, scheduledTime: Date, transcript: String, sinptomLog: DataLog, reportLog: DataLog) {
+    init (pacient: Pacient, scheduledTime: Date, transcript: String, sinptomLog: DataLog?, reportLog: DataLog?) {
         self.pacient = pacient
         self.scheduledTime = scheduledTime
         self.transcript = transcript
@@ -38,14 +38,20 @@ class Appointment: PersistenceObject {
             "smoking": pacient.smoking,
             "scheduledTime": scheduledTime,
             "transcript": transcript,
-            "dateSinp": sinptomLog.date,
-            "imagesSinp": sinptomLog.images,
-            "textsSinp": sinptomLog.texts,
-            "dateReport": reportLog.date,
-            "imagesReport": reportLog.images,
-            "textsReport": reportLog.texts,
             "isActive": true
         ]
+        
+        if let sinptomLog = sinptomLog {
+            self.dictInfo["dateSinp"] = sinptomLog.date
+            self.dictInfo["imagesSinp"] = sinptomLog.images
+            self.dictInfo["textsSinp"] = sinptomLog.texts
+        }
+        
+        if let reportLog = reportLog {
+            self.dictInfo["dateReport"] = reportLog.date
+            self.dictInfo["imagesReport"] = reportLog.images
+            self.dictInfo["textsReport"] = reportLog.texts
+        }
     }
     
     required init?(dictionary: [AnyHashable : Any]) {
@@ -61,21 +67,24 @@ class Appointment: PersistenceObject {
             let drink = dictionary["drink"] as? Bool,
             let hipertension = dictionary["hipertension"] as? Bool,
             let diabetes = dictionary["diabetes"] as? Bool,
-            let smoking = dictionary["smoking"] as? Bool,
-            let dateSinp = dictionary["dateSinp"] as? Date,
-            let imagesSinp = dictionary["imagesSinp"] as? [String],
-            let textsSinp = dictionary["textsSinp"] as? [String],
-            let dateReport = dictionary["dateReport"] as? Date,
-            let imagesReport = dictionary["imagesReport"] as? [String],
-            let textsReport = dictionary["textsReport"] as? [String] {
+            let smoking = dictionary["smoking"] as? Bool {
             
             self.scheduledTime = scheduledTime
             self.transcript = transcript
             
             self.pacient = Pacient(ID: pacientID, name: name, address: address, telephone: telephone, bornDate: bornDate, height: height, weight: weight, drink: drink, hipertension: hipertension, diabetes: diabetes, smoking: smoking)
             
-            self.reportLog = DataLog(date: dateReport, images: imagesReport, texts: textsReport)
-            self.sinptomLog = DataLog(date: dateSinp, images: imagesSinp, texts: textsSinp)
+            if let dateSinp = dictionary["dateSinp"] as? Date,
+                let imagesSinp = dictionary["imagesSinp"] as? [String],
+                let textsSinp = dictionary["textsSinp"] as? [String]{
+                self.sinptomLog = DataLog(date: dateSinp, images: imagesSinp, texts: textsSinp)
+            }
+            
+            if let dateReport = dictionary["dateReport"] as? Date,
+                let imagesReport = dictionary["imagesReport"] as? [String],
+                let textsReport = dictionary["textsReport"] as? [String] {
+                self.reportLog = DataLog(date: dateReport, images: imagesReport, texts: textsReport)
+            }
             
             self.isActive = true
             
