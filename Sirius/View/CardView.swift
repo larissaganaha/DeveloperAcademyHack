b5d9079e0af1f3685a591bfabf7be28c79b10037
Larissa Ganaha <larissaganaha@gmail.com>
2018-08-24 16:10:09 -0300

    Scroll View working

---
+++ Sirius/View/CardView.swift
@@ -0,0 +1,30 @@
+//
+//  CardView.swift
+//  Sirius
+//
+//  Created by Larissa Ganaha on 24/08/18.
+//  Copyright © 2018 Giovani Nascimento Pereira. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CardView: UIView {
+    @IBInspectable var cornerRadius: CGFloat = 10
+
+    @IBInspectable var shadowOffsetWidth: Int = 0
+    @IBInspectable var shadowOffsetHeight: Int = 3
+    @IBInspectable var shadowColor: UIColor? = UIColor.black
+    @IBInspectable var shadowOpacity: Float = 0.5
+
+    override func layoutSubviews() {
+        layer.cornerRadius = cornerRadius
+        let shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+
+        layer.masksToBounds = false
+        layer.shadowColor = shadowColor?.cgColor
+        layer.shadowOffset = CGSize(width: shadowOffsetWidth, height: shadowOffsetHeight);
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowPath = shadowPath.cgPath
+    }
+}
