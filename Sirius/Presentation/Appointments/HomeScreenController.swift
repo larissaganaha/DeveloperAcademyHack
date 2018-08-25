17335c62a574f6d6acaba2bfd10648663afa116f
Julianny Favinha <julianny.favinha@gmail.com>
2018-08-25 03:53:18 -0300

    Fetch data from firebase

--- Sirius/Presentation/Appointments/HomeScreenController.swift
+++ Sirius/Presentation/Appointments/HomeScreenController.swift
@@ -11,11 +11,14 @@ import UIKit
 class HomeScreenController: UIViewController {
 
     var categories = ["Consultas futuras", "Consultas passadas"]
-    var pacient:Pacient?
+    var pacient: Pacient?
 
-    var activeAppoints:[Appointment] = []
-    var unactiveAppoints:[Appointment] = []
+    var activeAppoints: [Appointment] = []
+    var unactiveAppoints: [Appointment] = []
     
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var lagTimeLabel: UILabel!
 
     override func viewDidLoad() {
@@ -27,8 +30,13 @@ class HomeScreenController: UIViewController {
                 
                 AppointmentMechanismFirebase.shared.retrieveAppointments(from: pacient.ID, completionHandler: { (appointments) in
                     if let appointments = appointments {
-                        self.activeAppoints = appointments.filter{ $0.isActive }
+                        let activeAppointsFilter = appointments.filter{ $0.isActive }
+                        self.activeAppoints = activeAppointsFilter.sorted(by: { (app1, app2) -> Bool in
+                            return app1.scheduledTime.compare(app2.scheduledTime) == ComparisonResult.orderedDescending
+                        })
                         self.unactiveAppoints = appointments.filter{ !$0.isActive}
+                        
+                        self.reloadNextAppointmentLabels()
                         
                         self.updateLagTimeIfNecessary()
                     }
@@ -39,7 +47,17 @@ class HomeScreenController: UIViewController {
         lagTimeLabel.clipsToBounds = true
     }
     
-    func updateLagTimeIfNecessary(){
+    private func reloadNextAppointmentLabels() {
+        if let pacient = self.pacient, let firstAppoint = self.activeAppoints.first {
+            self.welcomeLabel.text = "Olá, \(pacient.name.components(separatedBy: " ").first ?? pacient.name). Sua próxima consulta é:"
+            self.dateLabel.text = "Data: \(firstAppoint.scheduledTime.toString(dateFormat: "dd/MM/yyyy"))"
+            let hour = Calendar.current.component(.hour, from: firstAppoint.scheduledTime)
+            let minute = Calendar.current.component(.minute, from: firstAppoint.scheduledTime)
+            self.timeLabel.text = "Horário: \(hour)h\(minute)min"
+        }
+    }
+    
+    func updateLagTimeIfNecessary() {
         let dayOfNextAppoint = Calendar.current.component(.day, from: (self.activeAppoints.first?.scheduledTime)!)
         let currDay = Calendar.current.component(.day, from: Date())
         
@@ -91,7 +109,6 @@ extension HomeScreenController: UITableViewDelegate, UITableViewDataSource {
         return 130
     }
 
-
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = UIView()
         let titleLabel = UILabel()
@@ -107,6 +124,4 @@ extension HomeScreenController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 35
     }
-
-
 }
