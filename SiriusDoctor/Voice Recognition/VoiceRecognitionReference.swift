a074c44428b132dbc2a68b99b1ab4cfabfa07d04
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2018-08-25 05:05:12 -0300

    Updated vocabulary

--- SiriusDoctor/Voice Recognition/VoiceRecognitionReference.swift
+++ SiriusDoctor/Voice Recognition/VoiceRecognitionReference.swift
@@ -11,11 +11,11 @@ import Foundation
 class VoiceRecognitionReference {
     
     static let simptoms: [String] = [
-        "tosse", "dor de cabeça", "dor de ouvido", "diarréia", "tremedeira", "braço quebrado", "dor intermitente", "dor aguda", "secreção", "catarro", "tosse seca", "vermelhidão", "roxo", "coceira", "perda de cabelo", "perca de cabelo", "visão laser", "dor nas costas", "olhos vermelhos", "lágrima", "olho lacrimejando", "dor nas juntas", "dor no joelho", "dor nos joelhos", "visão embaçada", "vista embaçada", "visão turva", "dificuldade de respirar", "respiração forçada", "língua presa", "sangue", "nariz sangrando", "sangramento nasal", "alergia", "muito sensível", "hipersensibilidade", "dor nos olhos", "melanoma", "mancha na pele", "carcinoma"
+        "tosse", "dor de cabeça", "dor de ouvido", "diarréia", "tremedeira", "braço quebrado", "dor intermitente", "dor aguda", "secreção", "catarro", "tosse seca", "vermelhidão", "roxo", "coceira", "perda de cabelo", "perca de cabelo", "visão laser", "dor nas costas", "olhos vermelhos", "lágrima", "olho lacrimejando", "dor nas juntas", "dor no joelho", "dor nos joelhos", "visão embaçada", "vista embaçada", "visão turva", "dificuldade de respirar", "respiração forçada", "língua presa", "sangue", "nariz sangrando", "sangramento nasal", "alergia", "muito sensível", "hipersensibilidade", "dor nos olhos", "melanoma", "mancha na pele", "carcinoma", "cegueira"
     ]
     
     static let medicines: [String] = [
-        "cetoconazol", "xarope", "placebo", "amoxilina", "caminhada", "corrida", "natação", "chá", "pomada", "maracujá"
+        "cetoconazol", "xarope", "placebo", "amoxilina", "caminhada", "corrida", "natação", "chá", "pomada", "maracujá", "paracetamol", "analgésico", "fisioterapia", "ginástica", "rinossoro", "tala", "gesso", "engessar", "terapia", "acompanhamento psicológico", "anti inflamatório", "anti térmico", "anti-inflamatório", "anti-térmico", "antinflamatório", "antitérmico", "dipirona", "nimelisuda", "antibiótico", "naproxeno", "folic", "anticoncepcional", "contraceptivo", "vitamina c", "vitamina a", "vitamina b", "vitamina d", "complexo vitamínico"
     ]
     
     static func identifySimptoms(from text: String) -> [String] {
